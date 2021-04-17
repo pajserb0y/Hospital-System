fb0a419773d99c5a965f092b8c4ae4ee4b8f543a
GGmaz <kajtes.nikola@gmail.com>
2021-04-17 03:57:22 +0200

    chart #1

--- HospitalSystem/HospitalSystem/code/EditPatient.xaml.cs
+++ HospitalSystem/HospitalSystem/code/EditPatient.xaml.cs
@@ -22,11 +22,12 @@ namespace HospitalSystem.code
         {
             InitializeComponent();
             p = selectedPatient;
-               
+
+            txtID.Text = selectedPatient.Id.ToString();
             txtIme.Text = selectedPatient.FirstName;
             txtPrezime.Text = selectedPatient.LastName;
             txtJmbg.Text = selectedPatient.Jmbg.ToString();
-            txtAdresa.Text = selectedPatient.Adress;
+            txtAdress.Text = selectedPatient.Adress;
             txtTel.Text = selectedPatient.Telephone.ToString();
             txtEmail.Text = selectedPatient.Email;
             _ = selectedPatient.Gender == 'M' ? rbM.IsChecked = true : rbF.IsChecked = true;
@@ -38,7 +39,7 @@ namespace HospitalSystem.code
         private void txbSave_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             PatientsStorage.getInstance().Edit(new Patient(p.Id, txtIme.Text, txtPrezime.Text, Convert.ToInt64(txtJmbg.Text),
-                (char)((bool)rbF.IsChecked ? Convert.ToChar(rbF.Content) : Convert.ToChar(rbM.Content)), txtAdresa.Text, Convert.ToInt64(txtTel.Text), txtEmail.Text, cbGuest.IsChecked == true,
+                (char)((bool)rbF.IsChecked ? Convert.ToChar(rbF.Content) : Convert.ToChar(rbM.Content)), txtAdress.Text, Convert.ToInt64(txtTel.Text), txtEmail.Text, cbGuest.IsChecked == true,
                 txtUsername.Text, txtPassword.Text));
             this.Close();
         }
