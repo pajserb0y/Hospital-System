8361af688591866257e77018d2de64e6456e7e7d
GGmaz <kajtes.nikola@gmail.com>
2021-06-09 03:06:08 +0200

    mvvm finished

---
+++ HospitalSystem/HospitalSystem/code/View/NewJob.xaml.cs
@@ -0,0 +1,25 @@
+﻿using HospitalSystem.code.ViewModel;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HospitalSystem.code
+{
+    /// <summary>
+    /// Interaction logic for NewJob.xaml
+    /// </summary>
+    public partial class NewJob : Window
+    {
+        public NewJob(Patient selectedPatient)
+        {
+            InitializeComponent();
+            DataContext = new NewJobViewModel(selectedPatient, this, 1);
+        }
+
+        public NewJob(Patient selectedPatient, Job selectedJob)
+        {
+            InitializeComponent();
+            DataContext = new NewJobViewModel(selectedPatient, this, 2, selectedJob);
+        }        
+    }
+}
